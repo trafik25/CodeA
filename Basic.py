8d38b94a332be8d59daca2433b7a5ffe65989b62
Thomas Howard <thomashoward206@gmail.com>
2022-06-15 12:39:32 -0700

    1.2

--- Basic.py
+++ Basic.py
@@ -4,3 +4,20 @@ meal = "An english muffin"
 # Printing out breakfast
 print("Breakfast")
 print(meal)
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Bagel"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
