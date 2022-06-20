cecae8b37eb97d4ab1c6e4389b1b0e51b1db031b
Thomas Howard <thomashoward206@gmail.com>
2022-06-20 16:29:59 -0700

    1.1

---
+++ While_Loops.py
@@ -0,0 +1,19 @@
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+
+# i = 4
+# while i < 8:
+#   i += 1
+#   if i==3:
+#     continue
+#   print(i)
+
+i = 4
+while i < 8:
+  print(i)
+  i += 1
+else:
+  print("i is no longer here")
